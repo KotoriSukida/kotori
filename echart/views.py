a8e5049ff9ae4f676437bd3d538ca30e1e7abc2a
yinhao <yinhao023h@gmail.com>
2023-06-08 12:04:01 +0800

    新增接口文件

--- echart/views.py
+++ echart/views.py
@@ -1,7 +1,30 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
+
+from echart.models import Sale
 
 
 # Create your views here.
 
 def index(request):
+    # 返回主页面
     return render(request, 'echart/index.html')
+
+
+class SalesAPIVIew(View):
+    def get(self, request):
+        """
+        查询所有数据
+        路由：GET /books/
+        """
+        queryset = Sale.objects.all()
+        sale_list = []
+        for sale in queryset:
+            sale_list.append({
+                'id': sale.id,
+                'area': sale.area,
+
+            })
+        return JsonResponse(sale_list, safe=False,json_dumps_params={'ensure_ascii': False},
+)
